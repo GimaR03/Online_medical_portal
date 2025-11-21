c1e97f480fdbe126c519cce34da12e5d74d93f63
GimaR03 <it23673922@my.sliit.lk>
2025-11-21 21:49:05 +0530

    Update payment.php
    Update payment.php

--- payment.php
+++ payment.php
@@ -55,6 +55,7 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Payment Gateway Page -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment Gateway</title>
